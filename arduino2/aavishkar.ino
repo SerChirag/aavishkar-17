51f093f29b311fb1c90d6b0f9d3c97f3e59f224c
mishranant <anant.mishra.student@gmail.com>
2017-05-19 22:06:35 +0530

    Add files via upload

---
+++ arduino2/aavishkar.ino
@@ -0,0 +1,45 @@
+#include "config.h"
+#include "sms.h"
+#include "alcohol.h"
+#include "gyro.h"
+#include "seven.h"
+#include "gear.h"
+#include "BCD.h"
+#include "move.h"
+#include "functions.h"
+
+
+  void setup() 
+  {
+//    setupSonar();
+    setupMove();
+    setupGyro();
+    setupSeven();
+    setupAlcohol();
+    setupGSM();
+    setupBeep();
+  }
+  
+  void loop() 
+  {
+      /*updateGyro();
+      if(alcohol())
+      {
+          beep(1);
+      }
+      else
+      {*/
+         if(!BCDloop())
+          {
+              Serial.println("collision is imminent");
+              brake();
+          }
+          else
+          {
+            movementProcess();
+          }
+     // }
+
+    /* BCDloop();
+     delay(50);*/
+  }
---
+++ arduino2/functions.h
@@ -0,0 +1,9 @@
+void brake()
+{Steer(0,0);
+/* analogWrite( pwmlf , LOW );  
+  analogWrite( pwmlb , LOW );  
+  analogWrite( pwmrf , LOW );  
+  analogWrite( pwmrb , LOW );  
+*/
+}
+
